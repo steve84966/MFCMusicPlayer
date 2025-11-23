6b03879274a9863d7c649c48c51c48c21654506b
lucas150670 <lucas150670@petalmail.com>
2025-11-23 14:05:56 +0800

    feat: dialog layout update

--- resource.h
+++ resource.h
@@ -25,14 +25,16 @@
 #define IDC_LRCDISPLAY                  1011
 #define IDC_BUTTONTRANSLATION           1012
 #define IDC_BUTTON2                     1013
+#define IDC_MFCLINK1                    1014
+#define IDC_MFCLINKGITHUB               1014
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
